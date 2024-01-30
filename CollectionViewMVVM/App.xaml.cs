2db11b9c4af891a3b1629d6c10988c802c576f6c
User <User@LAB2-15>
2024-01-30 14:49:18 +0200

    Add project files.

---
+++ CollectionViewMVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectionViewMVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CollectionViewMVVM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionViewMVVM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
